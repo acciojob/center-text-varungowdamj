6dc51cbefa31423a5f812256eb66dcd091fe5ba1
Varun Gowda MJ <104577546+varungowdamj@users.noreply.github.com>
2024-05-25 22:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//your JS code here. If required.
 () => {
-  cy.get('h1').should('have.length.greaterThan', 0);
+  cy.visit('path-to-your-html-file'); // Ensure you have the correct path to your HTML file
   cy.get('center').should('have.length.greaterThan', 0);
+  cy.get('h1').should('have.length.greaterThan', 0);
 }

c22c0c34992ade83bc3b87ccdf1c628d1883576b
Varun Gowda MJ <104577546+varungowdamj@users.noreply.github.com>
2024-05-25 22:18:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 () => {
-  cy.visit('path-to-your-html-file'); // Ensure you have the correct path to your HTML file
-  cy.get('center').should('have.length.greaterThan', 0);
-  cy.get('h1').should('have.length.greaterThan', 0);
+  cy.visit('path-to-your-html-file', { timeout: 10000 }); // Increase timeout to 10 seconds
+  cy.get('center', { timeout: 10000 }).should('exist');
+  cy.get('center h1', { timeout: 10000 }).should('have.length.greaterThan', 0);
 }
